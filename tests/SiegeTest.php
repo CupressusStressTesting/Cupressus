6e282fa2786f8030ae34ca356fe7939d8a9babfe
ReenExe <ipourri@gmail.com>
2017-02-24 20:26:45 +0200

    next simple test (#1)

--- tests/SiegeTest.php
+++ tests/SiegeTest.php
@@ -16,5 +16,13 @@ class SiegeTest extends \PHPUnit_Framework_TestCase
                 'https://github.com/CupressusStressTesting/Cupressus/issues/1',
             ])
         );
+
+        $this->assertSame(
+            "https://github.com/CupressusStressTesting GET\nhttps://github.com/CupressusStressTesting/Cupressus GET",
+            $siege->convert([
+                'https://github.com/CupressusStressTesting',
+                'https://github.com/CupressusStressTesting/Cupressus',
+            ])
+        );
     }
 }
